33cf2c78955d8a45fdba4ec3c1021eb5cd99b180
Rob Ingram <rob@headcrash.org>
2020-03-04 12:30:09 +1300

    Render live data from Commerce.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import ProductRow from './ProductRow';
+import Commerce from '@chec/commerce.js';
+
+const commerce = new Commerce('pk_test_17836673c8ef939d955d255da8cc112a5560bf0a0cbca');
 
 class ProductList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      products: [],
+    }
+  }
+
+  componentDidMount() {
+    commerce.products.list().then((result) => {
+      this.setState({ products: result.data });
+    });
   }
 
   render() {
     return (
       <div className="container main-content">
-        <ProductRow />
-        <ProductRow />
+        {
+          this.state.products.map(product => {
+            return <ProductRow key={product.id} image={product.media.source} name={product.name} description={product.description} price={product.price.formatted_with_symbol} />
+          })
+        }
       </div>
     );
   }
--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import sampleImage from '../logo192.png';
 
-const ProductRow = () => {
+const ProductRow = ({ image, name, description, price }) => {
   return (
     <div className="row product">
       <div className="col-md-2">
-        <img src={sampleImage} alt="Sample Image" height="150" />
+        <img src={image} alt={name} height="150" />
       </div>
       <div className="col-md-8 product-detail">
-        <h4>Blue T-Shirt</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h4>{name}</h4>
+        <div dangerouslySetInnerHTML={{__html: description}}></div>
       </div>
       <div className="col-md-2 product-price">
-        $19.99
+        {price}
       </div>
     </div>
   );
